06e69b8d083ed9ec600847eaad73ebbf0d088912
Aliya Fatima <your-email@example.com>
2026-07-30 18:32:49 +0500

    Stage 3: create endpoint

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# This is our in-memory "database"
 tasks = [
     {"id": 1, "title": "Buy groceries", "done": False},
     {"id": 2, "title": "Learn FastAPI", "done": True},
     {"id": 3, "title": "Finish internship assignment", "done": False}
 ]
+
+# This defines the "shape" of the data we expect from the user
+class Task(BaseModel):
+    title: str
+    done: bool = False
 
 @app.get("/")
 def read_root():
@@ -18,17 +23,32 @@ def read_root():
 def health_check():
     return {"status": "ok"}
 
-# Endpoint to get ALL tasks
 @app.get("/tasks")
 def get_tasks():
     return tasks
 
-# Endpoint to get ONE specific task by its ID number
 @app.get("/tasks/{task_id}")
 def get_task(task_id: int):
     for task in tasks:
         if task["id"] == task_id:
             return task
-    # If the loop finishes and doesn't find it, return a 404 error
     return JSONResponse(status_code=404, content={"error": f"Task {task_id} not found"})
-#stage 2 done
+
+#stage 3 work
+@app.post("/tasks")
+def create_task(new_task: Task):
+    # Calculate the next available ID
+    new_id = len(tasks) + 1
+    
+    # Creating dictionary
+    task_dict = {
+        "id": new_id,
+        "title": new_task.title,
+        "done": new_task.done
+    }
+    
+    # Append it to list
+    tasks.append(task_dict)
+    
+    # Send the newly created task back to the client
+    return task_dict
